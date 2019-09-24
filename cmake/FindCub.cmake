0259ca2f5a26f170652876259fc8bd2353ee25f5
Rafael Radkowski <rafael@iastate.edu>
2019-09-24 08:59:44 -0500

    Cmake updates: - added cmake file to create the trackingx.lib - added cmake file to create the test applications.

--- cmake/FindCub.cmake
+++ cmake/FindCub.cmake
@@ -1,24 +1,41 @@
-# This script finds cub
-#Rafael Radkowski
+###############################################################################
+# Find CUB (CUDA Unbound)
+#
+# Exports the target CUB::CUB
+# Recommended method to link is using target_link_libraries(... CUB::CUB).
+# CUB is a header-only library, so it will just add to your include directories
+# No extra steps are required if using this method.
+# 
+# Alternatively, can also use old-style by making use of the following defined variables:
+# CUB_FOUND - True if CUB was found.
+# CUB_INCLUDE_DIRS - Directories containing the CUB include files.
 
+# In CMake 3.12+, set the CUB_ROOT environment variable to add another search path here
+find_path(
+    CUB_ROOT_DIR
+    NAMES cub/cub.cuh
+    PATHS "C:/SDK/cub-1.8.0"
+)
 
-set(CUB_INSTALL_PATHS_  "C:/SDK/cub-1.8.0"
-						"C:/SDK/cub-1.6.4" 
-						"C:/SDK/cub"  
-						"D:/SDK/cub-1.7.0"
-						"D:/SDK/cub-1.6.4"
-						)
+mark_as_advanced(
+    CUB_FOUND
+    CUB_INCLUDE_DIRS
+)
 
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    CUB
+    DEFAULT_MSG
+    CUB_ROOT_DIR
+)
 
-find_path (CUB_INCLUDE_DIR NAMES cub/cub.cuh
-		PATHS ${CUB_INSTALL_PATHS_} )
+if(CUB_FOUND)
+    set(CUB_INCLUDE_DIRS ${CUB_ROOT_DIR})
+endif()
 
-
-if (NOT CUB_INCLUDE_DIR) 
-	set (CUB_FOUND false)
-	 message("ERROR: CUB NOT found!")
-endif(NOT CUB_INCLUDE_DIR) 
-if ( CUB_INCLUDE_DIR) 
-	set (CUB_FOUND true)
-	 message("Found CUB in ${CUB_INCLUDE_DIR}.")
-endif( CUB_INCLUDE_DIR) 
+if(CUB_FOUND AND NOT TARGET CUB::CUB)
+    add_library(CUB::CUB INTERFACE IMPORTED)
+    set_target_properties(CUB::CUB PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CUB_INCLUDE_DIRS}"
+    )
+endif()
