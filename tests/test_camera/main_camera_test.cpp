5d8988fd95609504e12fa4dbd5fb1fcde380d2fb
Rafael Radkowski <rafael@iastate.edu>
2019-10-24 11:28:38 -0500

    Created a new branch for camera development and bug fixes.

---
+++ tests/test_camera/main_camera_test.cpp
@@ -0,0 +1,23 @@
+/*
+ Camera and point cloud test.
+
+
+
+*/
+
+// stl
+#include <iostream>
+#include <string>
+
+// local
+#include "StructureCoreCaptureDevice.h"  // the camera
+
+
+int main(int argc, char* argv)
+{
+	afrl::StructureCoreCaptureDevice* camera =  new afrl::StructureCoreCaptureDevice();
+
+	camera->
+
+	return 1;
+}
